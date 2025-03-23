3a00ee2b62af9d0f74402e6bd839a9cccf7529f8
Jacob <122307017+JacobHamelMottiez@users.noreply.github.com>
2025-03-22 22:58:39 -0400

    Update cleaning_references.R

--- R/cleaning_references.R
+++ R/cleaning_references.R
@@ -1,4 +1,11 @@
-
+#' Find the most common value in a vector
+#'
+#' This function determines the most frequently occurring value.
+#'
+#' @param references A tibble with references
+#' @return The most common value for each references that share the same unique identifier. 
+#' @export
+library(tidyverse)
 
 clean_references_fct <- function(references) {
   references <- reformat_references(references)
