2aa52ff33f4e98bb0d1aed791dcc3ca08b8804d9
Jacob <122307017+JacobHamelMottiez@users.noreply.github.com>
2025-03-22 22:51:55 -0400

    Update column_cleaning.R

--- R/column_cleaning.R
+++ R/column_cleaning.R
@@ -6,17 +6,29 @@
 #' @param references A dataframe of references
 #' @return A cleaned dataframe with formatted citations
 #' @export
-clean_articles_fct  <- function(references) {
+reformat_articles  <- function(articles) {
+  articles <- rename(articles, citing_id = eid, 
+                     citing_journal = publicationName, 
+                     citing_authors = authors_names, 
+                     citing_year = coverDate, 
+                     citing_title = title, 
+                     citing_doi = doi)
+  articles <- articles |> str_extract(citing_id, "(?<=2-s2\\.0-)[0-9]+"))
+
   
-
-
-clean_references_fct <- function(references) {
+reformat_references <- function(references) {
   references$sourcetitle <- toupper(references$sourcetitle)
-  references <- references %>%
-    rename(cited_id = id, citing_id = citing_eid, cited_title = sourcetitle) %>%
-    mutate(citing_id = str_extract(citing_id, "(?<=2-s2\\.0-)[0-9]+")) %>%
-    rename(cited_year = publicationyear)
+  references <- rename(references, 
+                       cited_id = id, 
+                       citing_id = citing_eid, 
+                       cited_title = sourcetitle, 
+                       cited_year = publicationyear)
+  
+  references <- references |> mutate(citing_id = str_extract(citing_id, "(?<=2-s2\\.0-)[0-9]+"))  
 
+
+Clean_references_fct <- function(references) {
+  references <- reformat_references(references)
   references <- references %>%
     group_by(cited_id) %>%
     mutate(
