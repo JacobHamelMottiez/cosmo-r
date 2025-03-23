46c3f2543a4535a3db3c6c35f82772a6a596fd65
Jacob <122307017+JacobHamelMottiez@users.noreply.github.com>
2025-03-22 22:55:34 -0400

    Update column_cleaning.R

--- R/column_cleaning.R
+++ R/column_cleaning.R
@@ -29,17 +29,3 @@ reformat_references <- function(references) {
   references <- references |> mutate(citing_id = str_extract(citing_id, "(?<=2-s2\\.0-)[0-9]+"))  
   return(references)
 }
-
-clean_references_fct <- function(references) {
-  references <- reformat_references(references)
-  references <- references %>%
-    group_by(cited_id) %>%
-    mutate(
-      title = most_common(title),
-      sourcetitle = most_common(authors),
-      publicationyear = most_common(cited_year)
-    ) %>%
-    ungroup() %>%
-    distinct()
-  return(references)
-}
