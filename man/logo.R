81b96b110b8bff0d4e098a91321136cd3b3534d3
Jacob <hamj0401@usherbrooke.ca>
2025-03-25 14:01:28 -0400

    Commit logos

--- man/logo.R
+++ man/logo.R
@@ -7,30 +7,31 @@ library(jpeg)
 # Sticker function---------------------------
 
 # Create your first sticker------------------
-img <- image_read("C:/Users/jacob/OneDrive - Université Laval/cosmo/man/figures/custom.png")
+img <- image_read("C:/Users/jacob/OneDrive - Université Laval/cosmo/man/figures/logo_1.png")
 
 fonts_dataset <- font_files()
 
 sticker(
   subplot = img,
   package = "cosmo-r",
-  s_width = 1,
-  s_height = 1,
+  s_width = 1.1,
+  s_height = 1.1,
   s_x = 1,
   s_y = 0.8,
-  p_size = 28,
-  h_fill = '#343b46',
-  h_color = '#ac87ff',
-  h_size = 1.5,
+  p_size = 35,
+  h_fill = '#2f303b',
+  h_color = '#ec9981',
+  h_size = 1,
   u_size = 5,
-  u_color = '#ac87ff',
+  u_color = '#4643b0',
   spotlight = F,
   l_y = 1,
   l_x = 1,
   l_width = 3,
   l_height = 3,
   l_alpha = 0.3,
-  p_color = '#ac87ff',
+  p_y = 1.5,
+  p_color = '#ec9981',
   p_family = "Times"
 ) %>% print()
 
