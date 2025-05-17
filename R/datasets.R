3e84853797542815dbc773655d22f286acbd826a
Jacob <hamj0401@usherbrooke.ca>
2025-05-17 16:43:14 -0400

    datasets

---
+++ R/datasets.R
@@ -0,0 +1,6 @@
+library(tidyverse)
+
+gpt_articles_dataset <- read_csv('data/gpt_articles_dataset.csv')
+gpt_references_dataset <- read_csv('data/gpt_references_dataset.csv')
+edge_list <- read_csv('data/edge_list.csv')
+node_list <- read_csv('data/node_list.csv')
