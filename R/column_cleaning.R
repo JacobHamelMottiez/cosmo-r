5a76848fd9cd3517c8b3bd7c6e57bda6d31bc004
Jacob <hamj0401@usherbrooke.ca>
2025-05-17 14:22:07 -0400

    update package

--- R/column_cleaning.R
+++ R/column_cleaning.R
@@ -7,25 +7,25 @@
 #' @return A cleaned dataframe with formatted citations
 #' @export
 reformat_articles  <- function(articles) {
-  articles <- rename(articles, 
-                     citing_id = eid, 
-                     citing_journal = publicationName, 
-                     citing_authors = authors_names, 
-                     citing_year = coverDate, 
-                     citing_title = title, 
+  articles <- rename(articles,
+                     citing_id = eid,
+                     citing_journal = publicationName,
+                     citing_authors = authors_names,
+                     citing_year = coverDate,
+                     citing_title = title,
                      citing_doi = doi)
-  articles <- articles |> str_extract(citing_id, "(?<=2-s2\\.0-)[0-9]+"))
+  articles <- articles |> str_extract(citing_id, "(?<=2-s2\\.0-)[0-9]+")
   return(articles)
 }
-  
+
 reformat_references <- function(references) {
   references$sourcetitle <- toupper(references$sourcetitle)
-  references <- rename(references, 
-                       cited_id = id, 
-                       citing_id = citing_eid, 
-                       cited_title = sourcetitle, 
+  references <- rename(references,
+                       cited_id = id,
+                       citing_id = citing_eid,
+                       cited_title = sourcetitle,
                        cited_year = publicationyear)
-  
-  references <- references |> mutate(citing_id = str_extract(citing_id, "(?<=2-s2\\.0-)[0-9]+"))  
+
+  references <- references |> mutate(citing_id = str_extract(citing_id, "(?<=2-s2\\.0-)[0-9]+"))
   return(references)
 }
